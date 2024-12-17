d330514fea6ed5bceb0b00be8fa363171986a7a5
rheamaerafol <rheamaerafol@gmail.com>
2024-12-17 16:03:42 +0800

    Initial commit

---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,66 @@
+import styles from './HeroStyles.module.css';
+import heroImg from '../../assets/profile.png';
+import sun from '../../assets/sun.svg';
+import moon from '../../assets/moon.svg';
+import facebookLight from '../../assets/facebook_light.svg';
+import facebookDark from '../../assets/facebook_dark.svg';
+import githubLight from '../../assets/github-light.svg';
+import githubDark from '../../assets/github-dark.svg';
+import linkedinLight from '../../assets/linkedin-light.svg';
+import linkedinDark from '../../assets/linkedin-dark.svg';
+import CV from '../../assets/cv.pdf';
+import { useTheme } from '../../common/ThemeContext';
+
+function Hero() {
+  const { theme, toggleTheme } = useTheme();
+
+  const themeIcon = theme === 'light' ? sun : moon;
+  const facebookIcon = theme === 'light' ? facebookLight : facebookDark;
+  const githubIcon = theme === 'light' ? githubLight : githubDark;
+  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+
+  return (
+    <section id="hero" className={styles.container}>
+      <div className={styles.colorModeContainer}>
+        <img
+          src={heroImg}
+          className={styles.hero}
+          alt="Profile picture of Rhea Rafol"
+        />
+        <img
+          className={styles.colorMode}
+          src={themeIcon}
+          alt="Color mode icon"
+          onClick={toggleTheme}
+        />
+      </div>
+      <div className={styles.info}>
+        <h1>
+          Rhea Mae
+          <br />
+          Rafol
+        </h1>
+        <h2>Aspiring Frontend Developer and UI/UX Designer</h2>
+        <span>
+          <a href="https://facebook.com/" target="_blank">
+            <img src={facebookIcon} alt="Facebook icon" />
+          </a>
+          <a href="https://github.com/" target="_blank">
+            <img src={githubIcon} alt="Github icon" />
+          </a>
+          <a href="https://linkedin.com/" target="_blank">
+            <img src={linkedinIcon} alt="Linkedin icon" />
+          </a>
+        </span>
+        <p className={styles.description}>
+          A 4th-year Computer Science student and have a strong passion for front-end development and UI/UX design. 
+        </p>
+        <a href={CV} download>
+          <button className="hover">Resume</button>
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
